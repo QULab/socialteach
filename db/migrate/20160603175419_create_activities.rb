dd79954c7be2e255c0bb41104e614fe8a7a875df
Thomas D.-S <hanniball1998@gmail.com>
2016-06-03 21:16:57 +0200

    wrong writing of references

--- db/migrate/20160603175419_create_activities.rb
+++ db/migrate/20160603175419_create_activities.rb
@@ -3,7 +3,7 @@ class CreateActivities < ActiveRecord::Migration
     create_table :activities do |t|
       t.string :name
       t.integer :levelpoints
-      t.reference :chapter, index: true
+      t.references :chapter, index: true
 
       t.timestamps null: false
     end
