7b94708b350dd5d4a001afb4120e56af75a301a3
Christina Palm <christina.palm@campus.tu-berlin.de>
2016-06-05 21:40:30 +0200

    Scope javascript loadGraph execution to graph partial

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( graph.css )
+Rails.application.config.assets.precompile += %w( graph.scss )
+Rails.application.config.assets.precompile += %w( graph.js   )
