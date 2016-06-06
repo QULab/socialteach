b2bef81f8c97e27547a38ec6451571cb10a92e6c
Sarah Köhler <sarah.c.koehler@gmail.com>
2016-06-07 00:40:08 +0200

    update activeAdmin: set predecessor and successor for chapters

--- app/admin/chapters.rb
+++ app/admin/chapters.rb
@@ -1,9 +1,21 @@
 ActiveAdmin.register Chapter do
 
+  form do |f|
+    f.inputs "Details" do
+      f.input :name
+      f.input :description
+      f.input :course_id
+      f.input :tier
+      f.input :shortname
+      f.input :predecessors
+      f.input :successors
+    end
+    f.actions
+  end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :name, :description, :course_id, :tier, :shortname, predecessor: [], successor: []
+permit_params :name, :description, :course_id, :tier, :shortname, predecessors: [], successors: []
 #
 # or
 #
