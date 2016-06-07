1269257590f48e67b1e42178aa0e6d523463e3e4
Christina Palm <christina.palm@campus.tu-berlin.de>
2016-06-07 22:08:35 +0200

    Add polymorphic association to questionnaire

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,4 +1,7 @@
 class Questionnaire < ActiveRecord::Base
   has_many :multiple_choice_questions
   has_many :completed_questionnaires
+
+  # difficulty_feedback, assessment, or excercise
+  belongs_to :qu_container, polymorphic: true
 end
--- db/migrate/20160607191227_create_questionnaires.rb
+++ db/migrate/20160607191227_create_questionnaires.rb
@@ -1,6 +1,8 @@
 class CreateQuestionnaires < ActiveRecord::Migration
   def change
     create_table :questionnaires do |t|
+      t.integer :qu_container_id # foreign key
+      t.string :qu_container_type # associated model (difficulty_feedback or activity)
 
       t.timestamps null: false
     end
