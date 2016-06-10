d1549ffc0d419fec7729cdd20de854dee67aa0c0
Christina Palm <christina.palm@campus.tu-berlin.de>
2016-06-10 11:45:01 +0200

    Delete questionnaire reference from difficulty_feedback

--- db/migrate/20160607092342_create_difficulty_feedbacks.rb
+++ db/migrate/20160607092342_create_difficulty_feedbacks.rb
@@ -1,8 +1,6 @@
 class CreateDifficultyFeedbacks < ActiveRecord::Migration
   def change
     create_table :difficulty_feedbacks do |t|
-      t.references :questionnaire, index: true
-      
       t.timestamps null: false
     end
   end
