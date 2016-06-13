cd08fe535c388c59a36413f96cb127784140463d
Sarah Köhler <sarah.c.koehler@gmail.com>
2016-06-14 01:16:53 +0200

    Revert "use markdown parser to display lecture text"
    This reverts commit 24e192a3f95cd9b88ca347e478ea08f2281efe30.

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -3,11 +3,4 @@ module ActivitiesHelper
     return "fa-book" if activity.content.is_a?(ActivityLecture)
     return "fa-question"
   end
-
-  # parses mardown and outputs html
-  def render_markdown(text)
-    renderer = Redcarpet::Render::HTML.new(escape_html: true, safe_links_only: true)
-    markdown = Redcarpet::Markdown.new(renderer, no_intra_emphasis: true, autolink: true, fenced_code_blocks: true)
-    return markdown.render(text).html_safe
-  end
 end
