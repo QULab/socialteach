d3b3c81de822f578caf88c85f85cd76a382187d6
Phuong Duy Nguyen <duyphuong@web.de>
2016-06-13 15:21:45 +0200

    routing fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160609213956) do
-
+ActiveRecord::Schema.define(version: 20160611115403) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
