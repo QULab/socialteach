1ad91274fc6518d97fd4d3c92687198ba71ead2d
Sarah Köhler <sarah.c.koehler@gmail.com>
2016-06-14 23:36:16 +0200

    helper method to check if user is enrolled in course

--- app/helpers/course_enrollments_helper.rb
+++ app/helpers/course_enrollments_helper.rb
@@ -1,2 +1,6 @@
 module CourseEnrollmentsHelper
+  def user_enrolled?(course, user)
+    enrollment = CourseEnrollment.where("user_id = ? AND course_id = ?", user.id, course.id).first
+    return enrollment.exists?
+  end
 end
