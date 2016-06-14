1540a9fe4c1c86e39b448a4273e1dfad0b82ac22
Sarah Köhler <sarah.c.koehler@gmail.com>
2016-06-15 00:08:50 +0200

    helpers to check if user is enrolled / finished an activity

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,6 +1,13 @@
 module ActivitiesHelper
   def activity_icon(activity)
     return "fa-book" if activity.content.is_a?(ActivityLecture)
+    # return "fa-graduation-cap" if activity.content.is_a?(ActivityAssessment)
+    # return "fa-check-square-o" if activity.content.is_a?(ActivityExercise)
     return "fa-question"
+  end
+
+  def user_completed_activity?(user, activity)
+    enrollment = user_enrollment(user, activity.course)
+    return ActivityStatus.where("activity_id = ? AND course_enrollment_id = ?", activity.id, enrollment.id).exists?
   end
 end
--- app/helpers/course_enrollments_helper.rb
+++ app/helpers/course_enrollments_helper.rb
@@ -1,6 +1,10 @@
 module CourseEnrollmentsHelper
-  def user_enrolled?(course, user)
+  def user_enrolled?(user, course)
+    return CourseEnrollment.where("user_id = ? AND course_id = ?", user.id, course.id).exists?
+  end
+
+  def user_enrollment(user, course)
     enrollment = CourseEnrollment.where("user_id = ? AND course_id = ?", user.id, course.id).first
-    return enrollment.exists?
+    return enrollment
   end
 end
