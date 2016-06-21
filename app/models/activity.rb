a60801789e2b99f9e1518a8ad3d16247a5ea4517
Sarah Köhler <sarah.c.koehler@gmail.com>
2016-06-21 22:44:22 +0200

    valid_predecessor methods for chapter and activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -24,11 +24,24 @@ class Activity < ActiveRecord::Base
   validates :name, :shortname, :tier, :chapter, presence: true
   validates :tier, :levelpoints, numericality: {greater_than_or_equal_to: 0}
 
+  # returns a list of all activities, that are valid predecessors for this activity
+  def valid_predecessors
+    chapter_activities = self.chapter.activities.to_a
+    predecessors = chapter_activities.select do |act|
+      act.tier < self.tier
+    end
+    return predecessors
+  end
+
   # Update switch statment as soon as other content classes exist!
   def color
     case content
       when ActivityLecture
         "#428CCA" # blue
+      when ActivityExercise
+        "#CAA82C" # yellow
+      when ActivityAssessment
+        "#99413F" # red
       else
         "#577B99" # greyish blue
     end
