57424218a4adea9c9a657c9ef4d7adc965c7707e
Sarah Köhler <sarah.c.koehler@gmail.com>
2016-06-30 00:24:49 +0200

    instructor can publish / unpublish course, unpublish only if no users enrolled

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,17 +1,26 @@
 class Course < ActiveRecord::Base
 
-    has_many :chapters
-    has_many :course_enrollments
-    belongs_to :users
-    has_one :feedback, as: :commentable
+  validate :cannot_unpublish_if_users_enrolled
+
+  has_many :chapters
+  has_many :course_enrollments
+  belongs_to :users
+  has_one :feedback, as: :commentable
 
 
-    has_many :activities, through: :chapters
+  has_many :activities, through: :chapters
 
-    def get_number_of_enrollments
-      CourseEnrollment.where("course_id = ?", self.id).count
+  def get_number_of_enrollments
+    CourseEnrollment.where("course_id = ?", self.id).count
+  end
+
   def self.all_published_courses
     where(published: true)
   end
+
+  def cannot_unpublish_if_users_enrolled
+    if (published_was && get_number_of_enrollments > 0 && !published)
+      self.errors[:base] << "There are Users enrolled in this course, you can not unpublish it."
     end
+  end
 end
