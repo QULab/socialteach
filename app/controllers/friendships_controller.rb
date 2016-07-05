4e1d5fd73f60d14be963e14172ed308a9a9f24ef
Duc <nguyen.duc@googlemail.com>
2016-07-06 01:20:54 +0200

    some cleanup works: no double friendships, no self friendship, no deleting friendships from others

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -25,6 +25,7 @@ class FriendshipsController < ApplicationController
   # POST /friendships.json
   def create
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+  
 
     respond_to do |format|
       if @friendship.save
@@ -61,6 +62,14 @@ class FriendshipsController < ApplicationController
       format.html { redirect_to user_path(current_user), notice: 'Removed friendship' }
       format.json { head :no_content }
     end
+  end
+
+  def find_friendship(user1, user2)
+    Friendship.find_by_user_id_and_friend_id(user1.id,user.id)
+  end
+
+  def find_facebookfriends
+
   end
 
   private
