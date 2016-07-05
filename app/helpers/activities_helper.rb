10c76a7bd46bd073c82f99e90ec0a3088c69e332
Christina Palm <christina.palm@campus.tu-berlin.de>
2016-07-05 12:37:14 +0200

    add buttons for adding questions and answers

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -13,4 +13,12 @@ module ActivitiesHelper
     markdown = Redcarpet::Markdown.new(renderer, no_intra_emphasis: true, autolink: true, fenced_code_blocks: true)
     return markdown.render(text).html_safe
   end
+
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, child_index: "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", f: builder)
+    end
+    link_to(name, "#", onclick: "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+  end
 end
