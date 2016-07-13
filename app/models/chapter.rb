b5db12e88e54257b398f2f1cf7c4300aef5352bf
Sarah Köhler <sarah.c.koehler@gmail.com>
2016-07-13 15:32:48 +0200

    chapter tiers get automatically assigned only the first time

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -23,30 +23,33 @@ class Chapter < ActiveRecord::Base
   validate :validate_tier
 
   before_validation :assign_tier
-  after_save :update_successor_tiers
+  #after_save :update_successor_tiers
 
   # returns a list of all chapters, that are valid predecessors for this chapter
   def valid_predecessors
-    return if tier_update_in_progress?
+    #return if tier_update_in_progress?
+    course_chapters = self.course.chapters.where.not(id: self.id).to_a
+    unless self.tier.present?
+      return course_chapters
+    end
 
     succ_min = self.successors.order(tier: :asc).first
 
-    course_chapters = self.course.chapters.where.not(id: self.id).to_a
     unless succ_min.nil?
       course_chapters.select! do |chapter|
-        chapter.tier < succ_min.tier
+        chapter.tier.present? && chapter.tier < succ_min.tier - 1
       end
     end
     course_chapters
   end
 
-  def signal_tier_update
-    @tier_update_in_progress = true
-  end
+  #def signal_tier_update
+    #@tier_update_in_progress = true
+  #end
 
-  def tier_update_in_progress?
-    @tier_update_in_progress
-  end
+  #def tier_update_in_progress?
+    #@tier_update_in_progress
+  #end
 
   private
 
@@ -60,32 +63,24 @@ class Chapter < ActiveRecord::Base
   def validate_tier
     succ_min = self.successors.order(tier: :asc).first
     if self.tier.present? and succ_min.present? and succ_min.tier <= self.tier
-      errors.add(:base, "Tier may not be higher than successor's tier")
+      errors.add(:base, "Tier may not be higher than or equal to successor's tier")
     end
   end
 
   def assign_tier
     max_pred = self.predecessors.order(tier: :desc).first
-    min_succ = self.successors.order(tier: :asc).first
-    if max_pred.nil?
-      if min_succ.present?
-        Rails.logger.info("#{self.name}: min succ is #{min_succ.name} with a tier of #{min_succ.tier}")
-        self.tier = min_succ.tier - 1
-      else
-        Rails.logger.info("#{self.name}: no succ or pred found, assigning tier 0")
-        self.tier = 0
+    if !max_pred.nil?
+      if max_pred.tier.present?
+        self.tier = max_pred.tier + 1
       end
-    else
-      Rails.logger.info("#{self.name}: max pred is #{max_pred.name} with a tier of #{max_pred.tier}")
-      self.tier = max_pred.tier + 1
     end
   end
 
-  def update_successor_tiers
-    self.successors.each do |chapter|
-      chapter.send(:assign_tier)
-      chapter.signal_tier_update
-      chapter.save!
-    end
-  end
+  #def update_successor_tiers
+    #self.successors.each do |chapter|
+      #chapter.send(:assign_tier)
+      #chapter.signal_tier_update
+      #chapter.save!
+    #end
+  #end
 end
