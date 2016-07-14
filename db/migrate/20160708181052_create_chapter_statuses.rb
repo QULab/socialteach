5af9042438650fce4a1e3b72f1fdf1e20e0d75fe
Sarah Köhler <sarah.c.koehler@gmail.com>
2016-07-14 17:40:24 +0200

    fix migrations

--- db/migrate/20160708181052_create_chapter_statuses.rb
+++ db/migrate/20160708181052_create_chapter_statuses.rb
@@ -2,11 +2,12 @@ class CreateChapterStatuses < ActiveRecord::Migration
   def change
     create_table :chapter_statuses do |t|
       t.boolean :skip, :default => false
+      t.boolean :finished, :default => false
       t.references :course_enrollment, index: true
       t.timestamps null: false
       t.float :difficultyFit, :default => 0.0
       t.integer :chapter_id, :default => 0
-      
+
     end
     add_foreign_key :chapter_statuses, :course_enrollments
   end
