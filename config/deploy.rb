7fbe9da3038d48ad9bb906e8e49facc87e14ac4f
Sarah Köhler <sarah.c.koehler@gmail.com>
2016-07-18 21:29:38 +0200

    update deploy.rb for sidekiq and linked_dirs for logs

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
-set :pty,             true
+set :pty,             false
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
@@ -16,8 +16,8 @@ set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_access_log, "#{shared_path}/log/puma.error.log"
+set :puma_error_log,  "#{shared_path}/log/puma.access.log"
 set :puma_threads,    [0, 16]
 set :ssh_options,     { forward_agent: true, user: fetch(:user) }
 set :puma_preload_app, true
@@ -37,7 +37,7 @@ set :branch,        ENV['BRANCH'] || :master
 ## Linked Files & Directories (Default None):
 set :linked_files, %w{config/database.yml}
 # set :linked_files, %w{config/secrets.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
