6fdd5e67f95664d76e6c6e80c826cb7db6817c0b
Christina Palm <christina.palm@campus.tu-berlin.de>
2016-07-19 19:58:38 +0200

    implement activity recommendations

--- app/models/course_enrollment.rb
+++ app/models/course_enrollment.rb
@@ -25,4 +25,118 @@ class CourseEnrollment < ActiveRecord::Base
   	total_activities = self.course.activities.distinct.count
   	return {:finished_activities => finished_activities, :total_activities => total_activities, :percent => finished_activities.to_f/total_activities.to_f}
   end
+
+  def recommended_activities
+
+    chapter = unless self.current_chapter.nil?
+                self.current_chapter
+              else
+                self.course.chapters.order(:tier).first
+              end
+
+    chapter_status = self.chapter_statuses.find_by(chapter: chapter)
+    chapter_activities = chapter.activities.order(:tier)
+
+    recommended = []
+    case chapter_status.difficultyFit
+
+    # good fit
+    when -0.9...0.9
+      # recommend activities of the current chapter
+      recommended += unfinished_activities(chapter)
+
+      # additionally recommend activities of following chapter(s) if not enough activities
+      recommended += successor_activities(chapter) if recommended.length < 5
+      successors = chapter.successors
+      while recommended.length < 5 && !successors.empty? do
+        next_successors = []
+        successors.each do |successor|
+          recommended += successor_activities(successor)
+          next_successors += successor.successors
+        end
+        successors = next_successors
+      end
+
+    # too hard
+    when -Float::INFINITY...-0.9
+      recommended += repeatable_activities(chapter)
+      # Recommend from earlier chapters if not enough activities
+      predecessors = chapter.predecessors
+        while recommended.length < 5 && !predecessors.empty?
+          next_predecessors = []
+          predecessors.each do |predecessor|
+            recommended += unfinished_activities(predecessor)
+            next_predecessors += predecessor.predecessors
+          end
+          predecessors.each do |predecessor|
+            recommended += repeatable_activities(predecessor)
+          end
+          predecessors = next_predecessors
+        end
+
+    # too easy
+    when 0.9..Float::INFINITY
+      recommended += successor_activities(chapter)
+      successors = chapter.successors
+      while recommended.length < 5 && !successors.empty?
+        next_successors = []
+        successors.each do |successor|
+          recommended += successor_activities(successor)
+          next_successors += successor.successors
+        end
+        successors = next_successors
+      end
+    end
+    recommended
+  end
+
+  private
+
+  # unfinished (unlocked) activities sorted by tier, difficulty
+  def unfinished_activities(chapter)
+    unfinished_activities = []
+    chapter.activities.each do |activity|
+      status = self.activity_statuses.find_by(activity: activity)
+      unless activity.locked?(self.user) || (!status.nil? && status.is_completed)
+        unfinished_activities.push(activity)
+      end
+    end
+
+    unfinished_activities.sort_by{|activity| [activity.tier, activity.difficulty]}
+  end
+
+  # completed activities (lectures, exercises), sorted desc by tier, (success/failed), difficulty
+  def repeatable_activities(chapter)
+    repeatable_activities = []
+    chapter.activities.each do |activity|
+      status = self.activity_statuses.find_by(activity: activity)
+      next if status.nil?
+      if status.is_completed && !activity.content.is_a?(ActivityAssessment)
+        repeatable_activities.push(activity)
+      end
+    end
+    repeatable_activities.sort_by{|activity| [activity.tier,
+                                              self.activity_statuses.find_by(activity: activity).status,
+                                              activity.difficulty]}
+  end
+
+  # activities of predecessors sorted desc by tier, completed, difficulty
+  def predecessor_activities(chapter)
+    predecessor_activities = []
+    chapter.predecessors.each do |predecessor|
+      predecessor_activities += predecessor.activities
+    end
+    predecessor_activities.sort_by{|activity| [-activity.tier,
+                                               self.activity_statuses.find_by(activity: activity).is_completed.to_s,
+                                               -activitiy.difficulty]}
+  end
+
+  # activities of successors sorted by tier, difficulty
+  def successor_activities(chapter)
+    successor_activities = []
+    chapter.successors.each do |successor|
+      successor_activities += successor.activities
+    end
+    successor_activities.sort_by{|activity| [activity.tier, activity.difficulty]}
+  end
 end
