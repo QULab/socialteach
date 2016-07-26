f4011dd03986e1a38f063fb8820b3260a3769712
Sarah Köhler <sarah.c.koehler@gmail.com>
2016-07-26 13:42:15 +0200

    remove explicit set of activity.difficulty in migration

--- db/migrate/20160708141458_add_difficulty_to_activity.rb
+++ db/migrate/20160708141458_add_difficulty_to_activity.rb
@@ -2,10 +2,4 @@ class AddDifficultyToActivity < ActiveRecord::Migration
   def change
     add_column :activities, :difficulty, :integer, null: false, default: 0
   end
-
-  Activity.find_each do |act|
-    unless act.difficulty.present?
-      act.update_attributes(:difficulty => 0)
-    end
-  end
 end
