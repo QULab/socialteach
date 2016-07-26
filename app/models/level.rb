8f71feaff5ff93ee2b22985ce09d3975d416f95d
Thomas D.-S <hanniball1998@gmail.com>
2016-07-26 22:56:11 +0200

    added restriction to level and level_pass

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,4 +1,7 @@
 class Level < ActiveRecord::Base
 	has_many :activities
 	has_many :course_enrollments
+
+	validates :level, :level_pass, numericality: {greater_than_or_equal_to: 0}
+	validates :level, :level_pass, uniqueness: true
 end
