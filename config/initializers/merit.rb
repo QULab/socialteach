008386a7beaa9254007ab569c563a9f6a7633849
Thomas Dittrich-Salamon <thomas@localhost.de>
2016-07-27 15:58:47 +0200

    added image of general badges

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -36,103 +36,103 @@ Merit::Badge.create!(
   id: 1,
   name: "5 Socialpoints",
   description: "Gained 5 socialpoints",
-  custom_fields: {condition: 5, type: :social}
+  custom_fields: {condition: 5, type: :social, image: "badge_1_5.png"}
 )
 
 Merit::Badge.create!(
   id: 2,
   name: "10 Socialpoints",
   description: "Gained 10 socialpoints",
-  custom_fields: {condition: 10, type: :social}
+  custom_fields: {condition: 10, type: :social, image: "badge_1_10.png"}
 )
 
 Merit::Badge.create!(
   id: 3,
   name: "25 Socialpoints",
   description: "Gained 25 socialpoints",
-  custom_fields: {condition: 25, type: :social}
+  custom_fields: {condition: 25, type: :social, image: "badge_1_25.png"}
 )
 
 Merit::Badge.create!(
   id: 4,
   name: "50 Socialpoints",
   description: "Gained 50 socialpoints",
-  custom_fields: {condition: 50, type: :social}
+  custom_fields: {condition: 50, type: :social, image: "badge_1_50.png"}
 )
 
 Merit::Badge.create!(
   id: 5,
   name: "100 Socialpoints",
   description: "Gained 100 socialpoints",
-  custom_fields: {condition: 100, type: :social}
+  custom_fields: {condition: 100, type: :social, image: "badge_1_100.png"}
 )
 
 Merit::Badge.create!(
   id: 6,
   name: "5 Learningpoints",
   description: "Gained 5 learningpoints",
-  custom_fields: {condition: 5, type: :learning}
+  custom_fields: {condition: 5, type: :learning, image: "badge_2_5.png"}
 )
 
 Merit::Badge.create!(
   id: 7,
   name: "10 Learningpoints",
   description: "Gained 10 learningpoints",
-  custom_fields: {condition: 10, type: :learning}
+  custom_fields: {condition: 10, type: :learning, image: "badge_2_10.png"}
 )
 
 Merit::Badge.create!(
   id: 8,
   name: "25 Learningpoints",
   description: "Gained 25 learningpoints",
-  custom_fields: {condition: 25, type: :learning}
+  custom_fields: {condition: 25, type: :learning, image: "badge_2_25.png"}
 )
 
 Merit::Badge.create!(
   id: 9,
   name: "50 Learningpoints",
   description: "Gained 50 learningpoints",
-  custom_fields: {condition: 50, type: :learning}
+  custom_fields: {condition: 50, type: :learning, image: "badge_2_50.png"}
 )
 
 Merit::Badge.create!(
   id: 10,
   name: "100 Learningpoints",
   description: "Gained 100 learningpoints",
-  custom_fields: {condition: 100, type: :learning}
+  custom_fields: {condition: 100, type: :learning, image: "badge_2_100.png"}
 )
 
 Merit::Badge.create!(
   id: 11,
   name: "5 Activities",
   description: "Completed successfully 5 Activities",
-  custom_fields: {condition: 5, type: :activities}
+  custom_fields: {condition: 5, type: :activities, image: "badge_3_5.png"}
 )
 
 Merit::Badge.create!(
   id: 12,
   name: "10 Activities",
   description: "Completed successfully 10 Activities",
-  custom_fields: {condition: 10, type: :activities}
+  custom_fields: {condition: 10, type: :activities, image: "badge_3_10.png"}
 )
 
 Merit::Badge.create!(
   id: 13,
   name: "25 Activities",
   description: "Completed successfully 25 Activities",
-  custom_fields: {condition: 25, type: :activities}
+  custom_fields: {condition: 25, type: :activities, image: "badge_3_25.png"}
 )
 
 Merit::Badge.create!(
   id: 14,
   name: "50 Activities",
   description: "Completed successfully 50 Activities",
-  custom_fields: {condition: 50, type: :activities}
+  custom_fields: {condition: 50, type: :activities, image: "badge_3_50.png"}
 )
 
 Merit::Badge.create!(
   id: 15,
   name: "100 Activities",
   description: "Completed successfully 100 Activities",
-  custom_fields: {condition: 100, type: :activities}
+  custom_fields: {condition: 100, type: :activities, image: "badge_3_100.png"}
 )
