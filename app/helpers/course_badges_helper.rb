dd38475d32d0bb5d26caed7b76cb195e69770cb1
Thomas D.-S <hanniball1998@gmail.com>
2016-07-29 21:48:41 +0200

    error with multiple checkbox if faild create

--- app/helpers/course_badges_helper.rb
+++ app/helpers/course_badges_helper.rb
@@ -2,14 +2,10 @@ module CourseBadgesHelper
 	def setup_course_badge(path)
 		position = path.index{|x| x.is_a? CourseBadge}
 		coursebadge = path[position]
-		(Activity.all - coursebadge.activities).each do |activity|
-			if activity.course.id == coursebadge.course_id
-				puts activity.name
+		(Activity.all - coursebadge.activities).each_with_index do |activity,i|
+			if activity.course.id == coursebadge.course_id and coursebadge.unlock_course_badges.select {|x| x.activity_id == activity.id}.empty?
 				coursebadge.unlock_course_badges.build(:activity => activity)
 			end
-		end
-		coursebadge.unlock_course_badges.each do |t|
-			puts t.activity.name
 		end
 		path = path
 	end
